a99e09e9730bba64940166d0e76182ccc80e91ff
Mintewab <demissiewminte123@gmail.com>
2019-08-20 15:12:04 -0700

    set up routing and example elements inside

---
+++ client/src/Pages/Jewelry.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Jewelry extends Component {
+    render() {
+        return(
+            <div> Jewelry and Watches ...</div>
+        );
+    }
+}
+
+export default Jewelry;
