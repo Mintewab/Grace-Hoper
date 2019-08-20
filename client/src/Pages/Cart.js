b315fe6d14c54f6cbcad3a49b73039d3028f320c
Mintewab <demissiewminte123@gmail.com>
2019-08-20 15:09:02 -0700

    set up routing and example elements inside

---
+++ client/src/Pages/Cart.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Cart extends Component {
+    render() {
+        return(
+            <div>Cart Page...</div>
+        );
+    }
+}
+
+export default Cart;
