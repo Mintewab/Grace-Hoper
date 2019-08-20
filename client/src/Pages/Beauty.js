f7a33964830f085c44874b8a7b91f81b37e85c82
Mintewab <demissiewminte123@gmail.com>
2019-08-20 15:08:26 -0700

    set up routing and example elements inside

---
+++ client/src/Pages/Beauty.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Beauty extends Component {
+    render() {
+        return(
+            <div> Beauty and Health.js...</div>
+        );
+    }
+}
+
+export default  Beauty;
