a1ca6e3581716f969dba91c2019936a046c22ff9
Mintewab <demissiewminte123@gmail.com>
2019-08-20 15:09:17 -0700

    set up routing and example elements inside

---
+++ client/src/Pages/ChildrenToys.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class ChildrenToys extends Component {
+    render() {
+        return(
+            <div> Children Toys...</div>
+        );
+    }
+}
+
+export default  ChildrenToys;
