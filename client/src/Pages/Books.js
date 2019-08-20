5b2114612ecce78ef192c1521dd352082189751e
Mintewab <demissiewminte123@gmail.com>
2019-08-20 15:08:44 -0700

    set up routing and example elements inside

---
+++ client/src/Pages/Books.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Books extends Component {
+    render() {
+        return(
+            <div> Books....</div>
+        );
+    }
+}
+
+export default  Books;
