4393b3fe84dc88c8c7538ff778a068c621d7c902
Mintewab <demissiewminte123@gmail.com>
2019-08-20 15:12:20 -0700

    set up routing and example elements inside

---
+++ client/src/Pages/LogIn.js
@@ -0,0 +1,18 @@
+
+class LogIn extends Component {
+    render() {
+        return(
+            <div>Login Page..
+                  {/* <div class="media">
+    <div class="media-body">
+         </div>
+    <div class="media-right">
+      <img src="img_avatar1.png" class="media-object" style="width:80px">
+    </div>
+  </div> */}
+                .</div>
+        );
+    }
+}
+
+export default LogIn;
