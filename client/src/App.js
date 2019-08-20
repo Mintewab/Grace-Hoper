b9ad6c632422cc7449e5baa6a664664d02a91226
Mintewab <demissiewminte123@gmail.com>
2019-08-20 15:34:53 -0700

    Router set

--- client/src/App.js
+++ client/src/App.js
@@ -34,6 +34,7 @@ function App() {
         <Route exact path="/Beauty" component={Beauty} />
         <Route exact path="/Books" component={Books} />
         <Route exact path="/ChildrenToys" component={ChildrenToys} />
+        <Route exact path="/Clothes" component={Clothes} />
 
 
 
