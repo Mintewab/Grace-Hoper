f131c896eb67acb216b9b2ab60cabcbb1b0810a6
Mintewab <demissiewminte123@gmail.com>
2019-08-20 15:11:43 -0700

    set up routing and example elements inside

---
+++ client/src/Pages/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Gallery from "../Containers/GalleryComponent";
+
+class Home extends Component {
+    render() {
+        return(
+            <Gallery />
+        );
+    }
+}
+
+export default Home;
