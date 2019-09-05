dd86362cf4eb4e6e1086c2afebf7bea217371eb6
Surafel Ferede <surafelaron@gmail.com>
2019-09-05 00:54:14 -0700

    some changes

--- client/src/Components/Card/Card.js
+++ client/src/Components/Card/Card.js
@@ -1,3 +1,18 @@
+<<<<<<< HEAD
+import React from "react";
+// import CardBtn from "../CardBtn";
+import "./Card.css";
+
+function Card(props) {
+  return (
+    <div className="card">
+     {props.children}
+    </div>
+  );
+}
+
+export default Card;
+=======
     
 // import React from "react";
 
@@ -14,3 +29,4 @@
 // }
 
 // export default Card;
+>>>>>>> master
--- client/src/Pages/Clothes.js
+++ client/src/Pages/Clothes.js
@@ -1,50 +1,141 @@
-import React, { Component } from "react";
-import API from "../Utils/APiClothes"
+// import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+import * as React from "react";
+import * as PropTypes from "prop-types";
+
+import FloatNav from "react-uwp/FloatNav";
+import IconButton from "react-uwp/IconButton";
+
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
-//import Card from "../Components/Card/Card"
 import "../../src/Components/Card/Card.css";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 
-class Clothes extends Component {
-
-    state = {
-        Clothes: []
-    };
-
-    componentDidMount(){
-        this.loadClothes();
-    }
-
-    loadClothes = () => {
-        API.getClothes()
-         .then(res=> this.setState({Clothes:res.data}))
-        .catch(err => console.log(err));
-    };
-
-    render(){
+class Clothes extends React.Component {
+    static contextTypes = { theme: PropTypes.object };
+    context: { theme: ReactUWP.ThemeType };
+  
+    render() {
+        const { theme } = this.context;
+        const staticButtonStyle: React.CSSProperties = {
+          background: theme.accent,
+          color: "#fff"
+        };
+    
         return (
-         <div>
-            {this.state.Clothes.map(Cloth => (
-                 <Card  key={Cloth._id} style={{ width: '20rem' }}>
-                 <Card.Img style={{ width: '10rem', height: "10rem"}}variant="top"src={Cloth.image} />
-                 <Card.Body>
-                   <Card.Title style={{ color: 'Black' }}>{Cloth.ProductName}</Card.Title>
-                   <Card.Text style={{ color: 'Black' }}>
-                   {Cloth.catagory  }
-                   
-                 
-                   </Card.Text >
-                   <Card.Text style={{ color: 'Black' }}>
-                   {Cloth.condition}                   
-                   </Card.Text >
-                   <Button variant="primary">Add to Carts</Button>
-                 </Card.Body>
-               </Card>
-            ))};
-            
-        </div>
-        ) 
-    }
+          <div>
+               
+            <FloatNav
+              style={{ margin: "20px 0" }}
+              isFloatRight={false}
+              focusItemIndex={1}
+              topNode={[
+               
+              ]}
+              expandedItems={[{
+                iconNode: (
+                  <IconButton hoverStyle={{}} activeStyle={{}}>
+                    RatingStarFillReducedPaddingHTMLLegacy
+                  </IconButton>
+                ),
+                title: "Add to Favorites"
+              }, {
+                iconNode: (
+                  <IconButton hoverStyle={{}} activeStyle={{}}>
+                    WebcamLegacy
+                  </IconButton>
+                ),
+                title: "My Account"
+              }, {
+                iconNode: (
+                  <IconButton hoverStyle={{}} activeStyle={{}}>
+                    HomeSolid
+                  </IconButton>
+                ),
+                title: "Jump to Home",
+                href: "/"
+              }]}
+              bottomNode={[
+           
+              ]}
+            />
+          
+        
+           <Container>
+ <Row>
+           <Card style={{ width: '20rem' }}>
+ <Card.Img style={{ width: '8rem' }}variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuvrqUWjYPDh6mQ2aD-MC9biKFQAWOeRJBiuyJTcWLtBcrdSsf" />
+ <Card.Body>
+   <Card.Title style={{ color: 'Black' }}>Clothes</Card.Title>
+   <Card.Text style={{ color: 'Black' }}>
+     Some quick example text to build on the Clothes and make up the bulk of
+     the card's content.
+   </Card.Text >
+   <Button variant="primary">Add to Carts</Button>
+ </Card.Body>
+</Card>
+<Card style={{ width: '20rem' }}>
+ <Card.Img style={{ width: '8rem' }}variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuvrqUWjYPDh6mQ2aD-MC9biKFQAWOeRJBiuyJTcWLtBcrdSsf" />
+ <Card.Body>
+   <Card.Title style={{ color: 'Black' }}>Clothes</Card.Title>
+   <Card.Text style={{ color: 'Black' }}>
+     Some quick example text to build on the Clothes and make up the bulk of
+     the card's content.
+   </Card.Text >
+   <Button variant="primary">Add to Carts</Button>
+ </Card.Body>
+</Card>
+<Card style={{ width: '20rem' }}>
+ <Card.Img style={{ width: '8rem' }}variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuvrqUWjYPDh6mQ2aD-MC9biKFQAWOeRJBiuyJTcWLtBcrdSsf" />
+ <Card.Body>
+   <Card.Title style={{ color: 'Black' }}>Clothes</Card.Title>
+   <Card.Text style={{ color: 'Black' }}>
+     Some quick example text to build on the Clothes and make up the bulk of
+     the card's content.
+   </Card.Text >
+   <Button variant="primary">Add to Carts</Button>
+ </Card.Body>
+</Card>
+<Card style={{ width: '20rem' }}>
+ <Card.Img style={{ width: '8rem' }}variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuvrqUWjYPDh6mQ2aD-MC9biKFQAWOeRJBiuyJTcWLtBcrdSsf" />
+ <Card.Body>
+   <Card.Title style={{ color: 'Black' }}>Clothes</Card.Title>
+   <Card.Text style={{ color: 'Black' }}>
+     Some quick example text to build on the Clothes and make up the bulk of
+     the card's content.
+   </Card.Text >
+   <Button variant="primary">Add to Carts</Button>
+ </Card.Body>
+</Card><Card style={{ width: '20rem' }}>
+ <Card.Img style={{ width: '8rem' }}variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuvrqUWjYPDh6mQ2aD-MC9biKFQAWOeRJBiuyJTcWLtBcrdSsf" />
+ <Card.Body>
+   <Card.Title style={{ color: 'Black' }}>Clothes</Card.Title>
+   <Card.Text style={{ color: 'Black' }}>
+     Some quick example text to build on the Clothes and make up the bulk of
+     the card's content.
+   </Card.Text >
+   <Button variant="primary">Add to Carts</Button>
+ </Card.Body>
+</Card><Card style={{ width: '20rem' }}>
+ <Card.Img style={{ width: '8rem' }}variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuvrqUWjYPDh6mQ2aD-MC9biKFQAWOeRJBiuyJTcWLtBcrdSsf" />
+ <Card.Body>
+   <Card.Title style={{ color: 'Black' }}>Clothes</Card.Title>
+   <Card.Text style={{ color: 'Black' }}>
+     Some quick example text to build on the Clothes and make up the bulk of
+     the card's content.
+   </Card.Text >
+   <Button variant="primary">Add to Carts</Button>
+ </Card.Body>
+</Card>
+</Row>
+</Container>
+</div>
+       );
+   }
 }
 
-export default Clothes;
+    
+
+export default  Clothes;
