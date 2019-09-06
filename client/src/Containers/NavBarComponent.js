5d5429d2c59cdec8b37f2a27dac27d990f110182
Surafel Ferede <surafelaron@gmail.com>
2019-09-06 15:45:41 -0700

    dashboard for user and admin

--- client/src/Containers/NavBarComponent.js
+++ client/src/Containers/NavBarComponent.js
@@ -1,25 +1,97 @@
-import React from 'react';
 import MenuBar from '../Components/MenuBarComponent';
 import "../Styles/NavBar.css";
-import { Link } from "react-router-dom";
+import React, { Fragment } from "react";
+import { Link, withRouter } from "react-router-dom";
+import { signout, isAuthenticated } from "../auth";
 
-class NavBar extends React.Component {
-
-    render() {
-        return (
-            <div>
-            <ul>                
-                <li><Link className="active" to="/">Home</Link></li>
-                <li><Link to="/SignUp">SignUp</Link></li>               
-                <li><Link to="/Login">Login</Link></li>
-                {/* <li><Link to="/About">About</Link></li> */}
-                <li><Link to="/cart">Cart</Link></li>
-                <li><Link to="/Search">Search</Link></li>
-            </ul>
-            <MenuBar />
-            </div>
-        );
+const isActive = (history, path) => {
+    if (history.location.pathname === path) {
+        return { color: "#ff9900" };
+    } else {
+        return { color: "#ffffff" };
     }
-}
+};
 
-export default NavBar;
+const NavBar = ({ history }) => (
+    <div>
+        <ul>
+            <li className="nav-item">
+                <Link
+                    className="nav-link"
+                    style={isActive(history, "/")}
+                    to="/"
+                >
+                    Home
+                </Link>
+            </li>
+
+            {isAuthenticated() && isAuthenticated().user.role === 0 && (
+                <li className="nav-item">
+                    <Link
+                        className="active"
+                        style={isActive(history, "/user/dashboard")}
+                        to="/user/dashboard"
+                    >
+                        Dashboard
+                    </Link>
+                </li>
+            )}
+
+            {isAuthenticated() && isAuthenticated().user.role === 1 && (
+                <li className="nav-item">
+                    <Link
+                        className="nav-link"
+                        style={isActive(history, "/admin/dashboard")}
+                        to="/admin/dashboard"
+                    >
+                        Dashboard
+                    </Link>
+                </li>
+            )}
+
+            {!isAuthenticated() && (
+                <Fragment>
+                    <li className="nav-item">
+                        <Link
+                            className="nav-link"
+                            style={isActive(history, "/signin")}
+                            to="/login"
+                        >
+                            Login
+                        </Link>
+                    </li>
+
+                    <li className="nav-item">
+                        <Link
+                            className="nav-link"
+                            style={isActive(history, "/signup")}
+                            to="/signup"
+                        >
+                            Signup
+                        </Link>
+                    </li>
+                </Fragment>
+            )}
+
+            {isAuthenticated() && (
+                <li className="nav-item">
+                    <span
+                        className="nav-link"
+                        style={{ cursor: "pointer", color: "#ffffff" }}
+                        onClick={() =>
+                            signout(() => {
+                                history.push("/");
+                            })
+                        }
+                    >
+                        Signout
+                    </span>
+                </li>
+            )}
+        </ul>
+        <MenuBar />
+    </div>
+);
+
+export default withRouter(NavBar);
+
--- client/src/Routes.js
+++ client/src/Routes.js
@@ -4,6 +4,10 @@ import NavBar from "./Containers/NavBarComponent"
 import Signup from "./Pages/SignUp";
 import LogIn from "./Pages/LogIn";
 import Home from "./Pages/Home";
+import PrivateRoute from "./auth/PrivateRoute";
+import ProfilePage from "./Pages/UserProfile";
+import AdminRoute from "./auth/AdminRoute";
+import AdminPage from "./Pages/AdminPage";
 const Routes = () => {
    return (
        <BrowserRouter>
@@ -13,6 +17,16 @@ const Routes = () => {
                <Route path="/" exact component={Home} />
                <Route path="/login" exact component={LogIn} />
                <Route path="/signup" exact component={Signup} />
+               <PrivateRoute
+                    path="/user/dashboard"
+                    exact
+                    component={ProfilePage}
+                />
+                <AdminRoute
+                    path="/admin/dashboard"
+                    exact
+                    component={AdminPage}
+                />
            </Switch>
         </div>
        </BrowserRouter>
