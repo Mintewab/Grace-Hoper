350af116d2d164d9ada287074c620bfa23385c5c
azdemissiew <ademssiew@gmail.com>
2019-09-06 18:42:52 -0700

    aded heroku

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
 const categoryRoutes = require("./routes/category");
 const productRoutes = require("./routes/product");
-const dotenv=require("dotenv").config()
+
 // app
 const app = express();
 
